1f42303b2f5dfcda64cfee68cb3f74329b0600cd
JasonLinn <cheng11220@gmail.com>
2017-05-01 01:21:26 +0800

    React

--- src/webpack-test/dist/js/bundle.js
+++ src/webpack-test/dist/js/bundle.js
@@ -72,8 +72,10 @@
 /***/ (function(module, exports) {
 
 function helloworld(){
-    
+    console.log('webpack run!!');
+    alert('webpack run123');
 }
+helloworld();
 
 /***/ })
 /******/ ]);
--- src/webpack-test/src/js/main.js
+++ src/webpack-test/src/js/main.js
@@ -1,3 +1,5 @@
 function helloworld(){
-    
+    console.log('webpack run!!');
+    alert('webpack run123');
 }
+helloworld();
