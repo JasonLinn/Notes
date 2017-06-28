dbc4e285c6d6063b1318f944b8a4df29768b3524
JasonLinn <cheng11220@gmail.com>
2017-06-28 18:41:03 +0800

    EE

--- src/Nodejs/04_module.exports/greet2.js
+++ src/Nodejs/04_module.exports/greet2.js
@@ -1,6 +1,7 @@
 exports.greet = function (){
     console.log('Hello!2');
 }
-
+//變異之前(greet.js)exports跟module.exports是不一樣的
+//mutate (變異之後exports和module.exports都會改變)
 console.log(exports);
 console.log(module.exports);
